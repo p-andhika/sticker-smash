0bf5a860afb62ce4ec418110abcbffba5eb90793
p.andhika <p.andhika@op.iitg.ac.in>
2025-02-06 10:42:39 +0700

    Add initial configuration files and components; remove unused files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    // Ensure this points to your source code
+    './app/**/*.{js,tsx,ts,jsx}',
+    // If you use a `src` directory, add: './src/**/*.{js,tsx,ts,jsx}'
+    // Do the same with `components`, `hooks`, `styles`, or any other top-level directories
+  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
